f3ea0906858dc8485f7ce3aaf930c82d0169b8bd
sergej.ruff <serijnh@gmail.com>
2023-10-11 19:56:54 +0200

    added a function that converts coloum characters to integer

--- code/convert_column_to_integers.py
+++ code/convert_column_to_integers.py
@@ -13,9 +13,12 @@ def convert_column_to_integers(df, column_name):
     :param column_name: coloumn with characters
     :return: dataframe with characters in coloumn being changed to integer.
     """
-    unique_values = df[column_name].unique()
+    unique_values = df[column_name].unique()  # extract unique characters
+    # creat a dictionary with unique colvalue as key and its index as value
     value_to_integer_mapping = {value: i for i, value in enumerate(unique_values)}
+    # replace each coloumn value with number(index) if character matches key.
     df[column_name] = df[column_name].map(value_to_integer_mapping)
+
 
 def convert_string_columns_to_integers(df):
     """
