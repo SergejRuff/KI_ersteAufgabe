a8c8ffa73bc6ed1d4d523a33cd5392b523dba96f
sergej.ruff <serijnh@gmail.com>
2023-10-11 19:45:23 +0200

    added a function that converts coloum characters to integer

--- code/erste_Aufgabe.py
+++ code/erste_Aufgabe.py
@@ -2,17 +2,12 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 import numpy as np
+from convert_column_to_integers import convert_string_columns_to_integers
 
 # import churn csv
 churn_df = pd.read_csv("../data/Churn.csv")
 
 # filter data first
-
-# replace Male/female with 0/1 for desicion tree
-churn_df["gender"] = churn_df["gender"].replace(["Male", "Female"], [0, 1])
-
-# replace yes/no with 0/1 for desicion tree
-churn_df = churn_df.replace(["No", "Yes"], [0, 1])
 
 # total charges is not numeric.
 churn_df["TotalCharges"] = churn_df["TotalCharges"].replace(' ', np.nan)
@@ -20,6 +15,13 @@ churn_df["TotalCharges"] = churn_df["TotalCharges"].replace(' ', np.nan)
 churn_df["TotalCharges"] = churn_df["TotalCharges"].astype(float)
 # now 11 NAs are introduced by transformation. should be removed
 churn_df = churn_df.dropna()
+
+# drop coloumns with useless information
+churn_df = churn_df.drop(labels=["customerID", "gender", "tenure",
+                                 "MonthlyCharges","TotalCharges"],axis=1)
+
+# call function to convert every unique character into integer
+convert_string_columns_to_integers(churn_df)
 
 
 # divide into training and test data: 80/20 %
